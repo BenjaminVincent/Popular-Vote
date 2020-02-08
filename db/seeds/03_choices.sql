484212aff40a13333115ed8bdac4758d4e4030e8
Max Rosenthal <mxrosenthal@yahoo.com>
2020-02-08 19:01:13 +0000

    separated database tabels into their own files with their own seed files.

---
+++ db/seeds/03_choices.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (1, 'Star Wars', 'space stuff!');
+INSERT INTO choices (poll_id, title, description)
+VALUES (1, 'Batman', 'bad guys!');
+INSERT INTO choices (poll_id, title, description)
+VALUES (1, 'Superman', 'disguises!');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (2, 'No where', 'anorexia!');
+INSERT INTO choices (poll_id, title, description)
+VALUES (2, 'Jimbo n Fools', 'rowdy dining.');
+INSERT INTO choices (poll_id, title, description)
+VALUES (2, 'the liquor store', 'We alcoholics.');
+
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Frank', 'Handsom');
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'David', 'Very Rich');
+INSERT INTO choices (poll_id, title, description)
+VALUES (3, 'Clive', 'fancy accent');
---
+++ db/seeds/04_votes.sql
@@ -0,0 +1,20 @@
+INSERT INTO votes (choice_id, rank)
+VALUES (1, 1);
+INSERT INTO votes (choice_id, rank)
+VALUES (1, 2);
+INSERT INTO votes (choice_id, rank)
+VALUES (1, 3);
+
+INSERT INTO votes (choice_id, rank)
+VALUES (2, 1);
+INSERT INTO votes (choice_id, rank)
+VALUES (2, 2);
+INSERT INTO votes (choice_id, rank)
+VALUES (2, 3);
+
+INSERT INTO votes (choice_id, rank)
+VALUES (3, 1);
+INSERT INTO votes (choice_id, rank)
+VALUES (3, 2);
+INSERT INTO votes (choice_id, rank)
+VALUES (3, 3);
