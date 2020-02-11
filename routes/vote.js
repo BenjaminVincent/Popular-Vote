dc68e6d41a15dc1d529156515b790330fa9fc945
Max Rosenthal <mxrosenthal@yahoo.com>
2020-02-11 17:56:18 +0000

    removed empty spaces

--- routes/vote.js
+++ routes/vote.js
@@ -4,24 +4,13 @@ const queries = require('../db/DBqueries')
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    // console.log('req.choice1', req.choice1)
-
     let templateVars = {};
-
-
-
-
     queries.getQuestion()
       .then(questions => {
-        // console.log('data: ', questions);
-
         templateVars.question = questions.question;
-
         queries.getChoices()
           .then(choice => {
-            console.log('choice: ', choice)
             templateVars.choices = choice;
-            // console.log('templateVars: ', templateVars)
           })
           .then(() => res.render('vote', templateVars));
       });
