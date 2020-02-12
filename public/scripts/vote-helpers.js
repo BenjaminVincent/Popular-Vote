7814b035a396bf757f3e63db6c9ce4d897a73a0a
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-12 02:14:11 +0000

    started drag/drop containers

--- public/scripts/vote-helpers.js
+++ public/scripts/vote-helpers.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
+  $('.box-item').draggable({
+    cursor: 'move',
+    helper: "clone"
+  });
+  $(".drop-votes").droppable({
+    drop: function(event, ui) {
+      var itemid = $(event.originalEvent.toElement).attr("itemid");
+      $('.box-item').each(function() {
+        if ($(this).attr("itemid") === itemid) {
+          $(this).appendTo(".drop-votes");
+        }
+      });
+    }
+  });
+
+
+  $(".drag-choices").droppable({
+    drop: function(event, ui) {
+      var itemid = $(event.originalEvent.toElement).attr("itemid");
+      $('.box-item').each(function() {
+        if ($(this).attr("itemid") === itemid) {
+          $(this).appendTo(".drop-votes");
+        }
+      });
+    }
+  });
 
 });
