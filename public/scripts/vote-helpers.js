fc486d6f13c4fdc7559308ead416ec83d5b8035a
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-12 03:19:38 +0000

    added some style

--- public/scripts/vote-helpers.js
+++ public/scripts/vote-helpers.js
@@ -3,24 +3,24 @@ $(document).ready(() => {
     cursor: 'move',
     helper: "clone"
   });
-  $(".drop-votes").droppable({
+  $("#drop-votes").droppable({
     drop: function(event, ui) {
       var itemid = $(event.originalEvent.toElement).attr("itemid");
       $('.box-item').each(function() {
         if ($(this).attr("itemid") === itemid) {
-          $(this).appendTo(".drop-votes");
+          $(this).appendTo("#drop-votes");
         }
       });
     }
   });
 
 
-  $(".drag-choices").droppable({
+  $("#drag-choices").droppable({
     drop: function(event, ui) {
       var itemid = $(event.originalEvent.toElement).attr("itemid");
       $('.box-item').each(function() {
         if ($(this).attr("itemid") === itemid) {
-          $(this).appendTo(".drop-votes");
+          $(this).appendTo("#drop-votes");
         }
       });
     }
