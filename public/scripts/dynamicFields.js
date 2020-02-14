0868d18ce25c9a4c3fc1d2187f2f9c75c6044d35
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-14 08:16:15 +0000

    still working on style

--- public/scripts/dynamicFields.js
+++ public/scripts/dynamicFields.js
@@ -6,7 +6,7 @@ function addInputField() {
   var divtest = document.createElement("div");
   divtest.setAttribute("class", "option removeclass" + room);
   var rdiv = 'removeclass' + room;
-  divtest.innerHTML = '<input type="text" class="form-control" form="poll-question" id="choice" name="choice" value ="" placeholder="choice"><input type="text" class="form-control" form="poll-question" id="description" name="description" value="" placeholder="description"><div class="input-group-btn"><button class="btn btn-danger" type="button" onclick="remove_education_fields(' + room + ');"><span class="glyphicon glyphicon-minus" aria-hidden="true">Remove</span></button></div><div class="clear"></div>';
+  divtest.innerHTML = '<input type="text" class="form-control" form="poll-question" id="choice" name="choice" value ="" placeholder="option"><input type="text" class="form-control" form="poll-question" id="description" name="description" value="" placeholder="description"><div class="input-group-btn"><button class="btn btn-danger" type="button" onclick="remove_education_fields(' + room + ');"><span class="glyphicon glyphicon-minus" aria-hidden="true">Remove</span></button></div><div class="clear"></div>';
 
   objTo.appendChild(divtest)
 }
